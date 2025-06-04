b73c68788de261e6ac154c387d6ba961a632a0ad
Adem Bayraktar <ademsbayraktar@gmail.com>
2025-06-04 22:04:50 +0100

    added display of events as they are added - events are in chronological order (by start) - delete buttons appear next to each as they're created - also fixed a bug with saving which I introduced last commit

--- shift_calendar_generator.py
+++ shift_calendar_generator.py
@@ -12,7 +12,8 @@ from uuid import UUID, uuid4
 from PyQt6.QtCore import QTime
 from PyQt6.QtWidgets import (QApplication, QCalendarWidget, QFileDialog,
                              QGridLayout, QHBoxLayout, QLabel, QMainWindow,
-                             QPushButton, QTimeEdit, QVBoxLayout, QWidget)
+                             QPushButton, QStyle, QTimeEdit, QVBoxLayout,
+                             QWidget)
 
 log = logging.getLogger(Path(__file__).name)
 logging.basicConfig(level=logging.INFO)
@@ -51,6 +52,20 @@ class Event:
 
         return '\n'.join(content)
 
+    def gui_str(self) -> str:
+        """formats the start date as a nice str for the gui"""
+        return (
+            f'{self.dtstart.strftime('%d %b %Y')} '
+            f'{self.dtstart.strftime('%I:%M %p')} - '
+            f'{self.dtend.strftime('%I:%M %p')}'
+        )
+
+    def __lt__(self, other: 'Event') -> bool:
+        return self.dtstart < other.dtstart
+
+    def __eq__(self, other: 'Event') -> bool:
+        return self.dtstart == other.dtstart
+
 
 @dataclass
 class Calendar:
@@ -68,7 +83,7 @@ class Calendar:
         content = ['BEGIN:VCALENDAR']
         content.append(f'VERSION:{self.version}')
         content.append(f'PRODID:{self.prodid}')
-        for event in self.events:
+        for event in sorted(self.events):
             content.append(event.to_str())
         content.append('END:VCALENDAR')
 
@@ -82,6 +97,38 @@ class Calendar:
         log.info(f'saved to "{save_to}"')
 
 
+class EventLayout(QHBoxLayout):
+    """stores a label for showing the event time and a delete button"""
+    def __init__(self, event: Event, delete_func: callable):
+        super().__init__()
+        self.event = event
+        self.delete_func = delete_func
+
+        label = QLabel()
+        label.setText(event.gui_str())
+        delete_button = QPushButton()
+        bin_pxmap = QStyle.StandardPixmap.SP_DialogCancelButton
+        bin_icon = delete_button.style().standardIcon(bin_pxmap)
+        delete_button.setIcon(bin_icon)
+        delete_button.setFixedWidth(50)
+        delete_button.clicked.connect(self.delete_this_event)
+
+        self.widgets = [label, delete_button]
+        for widget in self.widgets:
+            self.addWidget(widget)
+
+    def remove(self) -> None:
+        """deletes the label and button widgets for removal"""
+        for widget in self.widgets:
+            self.removeWidget(widget)
+
+    def delete_this_event(self) -> None:
+        """deletes this event from the list of shifts"""
+        self.remove()
+        self.delete_func()
+        log.info(f'deleted event: {self.event.gui_str()}')
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -89,6 +136,8 @@ class MainWindow(QMainWindow):
 
         layout = QHBoxLayout()
         controls = QVBoxLayout()
+        self.shifts = QVBoxLayout()
+        self.shift_events = []
 
         add_button = QPushButton('Add event')
         add_button.clicked.connect(self.add_event)
@@ -111,9 +160,9 @@ class MainWindow(QMainWindow):
 
         time_grid = QGridLayout()
         start_label = QLabel()
-        start_label.setText('Shift start time:')
+        start_label.setText('Start time:')
         end_label = QLabel()
-        end_label.setText('Shift end time:')
+        end_label.setText('End time:')
         time_grid.addWidget(start_label, 0, 0)
         time_grid.addWidget(self.start_time_input, 0, 1)
         time_grid.addWidget(end_label, 1, 0)
@@ -127,7 +176,15 @@ class MainWindow(QMainWindow):
         controls.addWidget(del_button)
         controls.addWidget(add_button)
 
+        shifts_title = QLabel()
+        shifts_title.setFixedWidth(300)
+        shifts_title.setText('Shifts:')
+        self.shifts.addWidget(shifts_title)
+        # add a streched empty widget for padding
+        self.shifts.addWidget(QWidget(), 1)
+
         layout.addLayout(controls)
+        layout.addLayout(self.shifts)
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -157,21 +214,47 @@ class MainWindow(QMainWindow):
             summary='long day'
         )
         self.calendar.events.append(event)
-        log.info(f'added {event=}')
+        self.shift_events.append(
+            EventLayout(event, self.get_delete_func(event)))
+        self.update_shifts()
+        log.info(f'added {event.gui_str()=}')
 
     def delete_last_event(self) -> None:
         """deletes the most recently added event from the calendar"""
         if len(self.calendar.events) > 0:
-            self.calendar.events.pop()
+            event = self.calendar.events.pop()
+            for event_layout in self.shift_events:
+                if event_layout.event == event:
+                    break
+            self.update_shifts()
             log.info('deleted last event')
+
+    def get_delete_func(self, event: Event) -> callable:
+        """returns a func that an EventLayout can use to delete itself"""
+        def delete_func() -> None:
+            self.calendar.events.remove(event)
+            self.update_shifts()
+        return delete_func
 
     def save_to_file(self) -> None:
         """opens a file dialog and saves the file to the specified filename"""
         from_dialog = self.save_dialog.getSaveFileName()[0]
         if from_dialog != '':
-            self.calendar.to_file(Path())
+            self.calendar.to_file(Path(from_dialog))
         else:
             log.warning('Save dialog closed. File not saved!')
+
+    def update_shifts(self) -> None:
+        """removes and re-adds the shift events"""
+        for event_layout in self.shift_events:
+            event_layout.remove()
+            self.shifts.removeItem(event_layout)
+        self.shift_events = []
+        for event in sorted(self.calendar.events):
+            event_layout = EventLayout(event, self.get_delete_func(event))
+            # insert widgets 1 before last as it is a padding widget
+            self.shifts.insertLayout(self.shifts.count() - 1, event_layout)
+            self.shift_events.append(event_layout)
 
 
 app = QApplication(sys.argv)
